b6159b97a2a8388f89fd3d08e865655ba3952934
divyakp88 <kpdivya08@gmail.com>
2025-08-08 15:27:40 +0530

    print palindrome triangle with 2 lines of code

---
+++ palindrome_triangle.py
@@ -0,0 +1,14 @@
+#ou are given a positive integer .
+#Your task is to print a palindromic triangle of size .
+#For example, a palindromic triangle of size  is:
+#1
+#121
+#12321
+#1234321
+#123454321
+#You can't take more than two lines.
+#Using anything related to strings will give a score of 0.
+#Using more than one for-statement will give a score of 0.
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**i-1)//9**2)
